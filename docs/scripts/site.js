acdbbe66970e23a4a701ad4276b396ab1edab334
Cody Sherman <meatwaddancin@gmail.com>
2018-05-06 05:21:50 +0000

    chore(docs): remove query parameter for home

--- docs/scripts/site.js
+++ docs/scripts/site.js
@@ -1,49 +1,52 @@
 let getURLParameter = (name) => {
-    return (
-        decodeURIComponent(
-            (new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [
-                null,
-                '',
-            ])[1].replace(/\+/g, '%20'),
-        ) || null
-    );
+  return (
+    decodeURIComponent(
+      (new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [
+        null,
+        '',
+      ])[1].replace(/\+/g, '%20'),
+    ) || null
+  );
 };
 
 let replaceQueryParam = (param, newval, search) => {
-    let regex = new RegExp('([?;&])' + param + '[^&;]*[;&]?');
-    let query = search.replace(regex, '$1').replace(/&$/, '');
-
-    return (query.length > 2 ? query + '&' : '?') + (newval ? param + '=' + newval : '');
+  let regex = new RegExp('([?;&])' + param + '[^&;]*[;&]?');
+  let query = search.replace(regex, '$1').replace(/&$/, '');
+  
+  return (query.length > 2 ? query + '&' : '?') + (newval ? param + '=' + newval : '');
 };
 
 let setURLParameter = (tabName) => {
-    window.history.pushState(null, null, replaceQueryParam('tab', tabName, window.location.search));
+  window.history.pushState(null, null, tabName ? replaceQueryParam('tab', tabName, window.location.search) : '/');
 };
 
 let setActiveTab = () => {
-    let tab = getURLParameter('tab');
-    if (tab) {
-        window.letsgo.remove('.main-tab', 'disabled').add(`#main-tab-${tab}`, 'disabled=disabled');
-        window.letsgo.hide('.main-section').show(`#${tab}`);
-    }
+  let tab = getURLParameter('tab');
+  if (tab) {
+    window.letsGo.remove('.main-tab', 'disabled').add(`#main-tab-${tab}`, 'disabled=disabled');
+    window.letsGo.hide('.main-section').show(`#${tab}`);
+  }
 };
 
 window.onload = () => {
-    setActiveTab();
-    document
-        .getElementById('main-tab-about')
-        .addEventListener('click', () => setURLParameter('about'));
-    document
-        .getElementById('main-tab-setup')
-        .addEventListener('click', () => setURLParameter('setup'));
-    document
-        .getElementById('main-tab-commands')
-        .addEventListener('click', () => setURLParameter('commands'));
-    document
-        .getElementById('main-tab-animations')
-        .addEventListener('click', () => setURLParameter('animations'));
+  setActiveTab();
+  document
+    .getElementById('header-logo')
+    .addEventListener('click', () => setURLParameter());
+  document
+    .getElementById('main-tab-about')
+    .addEventListener('click', () => setURLParameter());
+  document
+    .getElementById('main-tab-setup')
+    .addEventListener('click', () => setURLParameter('setup'));
+  document
+    .getElementById('main-tab-commands')
+    .addEventListener('click', () => setURLParameter('commands'));
+  document
+    .getElementById('main-tab-animations')
+    .addEventListener('click', () => setURLParameter('animations'));
 };
 
 window.onpopstate = (e) => {
-    setActiveTab();
+  setActiveTab();
 };
